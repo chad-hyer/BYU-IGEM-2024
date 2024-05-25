29c309683bf79ab0dc69ece6160be39c840ea55b
Kaden Parker <bobert11@byu.edu>
2024-05-25 15:45:17 -0600

    removed large files

--- src/testing.py
+++ src/testing.py
@@ -1,24 +1,65 @@
 import numpy as np
 from PIL import Image
 import pandas
+import matplotlib.pyplot as plt
+import os
+import csv
 # import imageio as iio
+
+
+""" This file is expecting images to be JPEG's."""
+
+
+"""Downloaded text image set from Kaggle at: https://www.kaggle.com/datasets/pavansanagapati/images-dataset"""
 
 # test_img=iio.v2.imread("data/test_photos/red_plant.jpeg",pilmode='RGB')
 
 test_im=np.array(Image.open("data/test_photos/red_plant.jpeg"))
-red_values=test_im[:,:,0]
-print(red_values)
-num_rows, num_columns = red_values.shape
-print(test_im.shape)
-print(f"Red values array dimensions: {num_rows} rows × {num_columns} columns")
 
-# print(test_im)
-# print(test_im.size)
-# Image.open("data/test_photos/red_plant.jpeg").show()
+# print(red_values)
 
 
+"""Returns the average red pixel value of a jpeg"""
+def Redify(imagey):
 
-#9:20
+    red_image_vals=imagey[:,:,0]
+    num_rows, num_columns = red_image_vals.shape
+    redvalue=0
+    for i in range(num_rows):
+        for j in range(num_columns):
+            redvalue+=red_image_vals[i][j]
+    # print(redvalue/(num_rows*num_columns))
+    return(redvalue/(num_rows*num_columns))
+# 15min+2:30
+def Redify2(imagey):
+    red_image_vals=imagey[:,:,0]
+    num_rows, num_columns = red_image_vals.shape
+    redvalue=np.sum(red_image_vals)
+    return redvalue/(num_rows*num_columns)
+
+
+
+
+def process_image(input_folder):
+    image_files=[name for name in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder,name))]
+    with open('test_data.csv','w', newline='') as csvfile:
+        writer=csv.writer(csvfile)
+        writer.writerow(['Name','Red Value'])
+        for image_name in image_files:
+            image_path = os.path.join(input_folder,image_name)
+            image_array = np.array(Image.open(image_path))
+            red_value = Redify2(image_array)
+            writer.writerow([image_name,red_value])
+        
+    return
+
+process_image("/Users/kadenparker/Desktop/Misc/BYU-IGEM-2024/src/data/test_photos/data/cars")
+
+Redify(test_im)
+
+
+
+#2:42-2:54
 
 #APP should automatically change in settings the camera capture in IOS from high efficiency to most compatible
 
@@ -28,4 +69,6 @@ print(f"Red values array dimensions: {num_rows} rows × {num_columns} columns")
 
 
 if __name__=="__main__":
-    pass
+    print("starting")
+    process_image("/Users/kadenparker/Desktop/Misc/BYU-IGEM-2024/src/data/test_photos/data/flowers")
+    print("Image processing completed successfully")
