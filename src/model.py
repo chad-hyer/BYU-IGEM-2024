8d47bc4f03fe030bd3c9ce3cf59c8c0a15b2a5a1
Kaden Parker <131911150+KadenParker1@users.noreply.github.com>
2024-09-29 19:50:23 -0600

    Update model.py

--- src/model.py
+++ src/model.py
@@ -4,18 +4,23 @@ import csv
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pandas as pd
+import joblib
 
-file="test_data.csv"
-data=pd.read_csv(file)
-X=data[["Name"]]
-print(X.shape)
-Y=data["Red Value"]
+
+file = "test_data.csv" #Path to data csv, will be in the form of amount of chemical, and red pixel values of image
+data = pd.read_csv(file) #read in data
+X = data[["Name"]]
+
+
+Y = data["Red Value"]
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-model=LinearRegression()
+model = LinearRegression()
 model.fit(X_train, Y_train)
-# model.predict([[]])
-print("model set")
-red_value_to_predict=10
+joblib.dump(model, 'AI_chemical_in_soil_predicting_model.joblib')
+
+
+#testing model
+red_value_to_predict = 10
 predicted_chemical_amount = model.predict([[red_value_to_predict]])
 print(f"Predicted chemical amount for red value {red_value_to_predict}: {predicted_chemical_amount[0]}")
 
